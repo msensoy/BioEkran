81b27f06d2329c0c80f95a73c87cdc79a42aa8d2
Mehmet Şensoy <mehmet.sensoy@outlook.com>
2020-07-03 18:54:03 +0300

    cmt

---
+++ Proje/SignalRSample/Uygulama/Controllers/RegistrationController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Uygulama.Services.Data;
+using Microsoft.AspNetCore.Mvc;
+using Uygulama.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Uygulama.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var apiService = new ApiServices();
+                var token = await apiService.RegisterUserAsync(user.Email, user.PhoneNumber, user.Password, user.Password);
+                if (!string.IsNullOrEmpty(token))
+                {
+                    HttpContext.Session.SetString("accesstoken", token);
+                    return RedirectToAction("Index", "MainScreen");
+                }
+            }
+            else
+            {
+                ViewData["Hata"] = "Lütfen boş bırakmayınız";
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ Proje/SignalRSample/Uygulama/RegisterBindingModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Uygulama.Models
+{
+    public class RegisterBindingModel
+    {
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
